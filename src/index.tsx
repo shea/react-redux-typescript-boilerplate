90d3769fa13fffbe1d56ae72ef603d9117b15c28
Shea <shea.sullivan@gmail.com>
2019-02-03 10:44:39 -0800

    Fix dependencies and convert from yarn to npm

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,6 @@ import Main from './main'
 import * as serviceWorker from './serviceWorker'
 import configureStore from './configureStore'
 
-import 'typeface-ibm-plex-sans'
 import './styles'
 
 // We use hash history because this example is going to be hosted statically.
